6674e41a1b826d06485f89f0d5d1166eb9831b5b
pome-ta <pome-ta@users.noreply.github.com>
2021-05-12 20:05:10 +0900

    音着火

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,11 +7,15 @@ osc.connect(actx.destination);
 osc.start();
 console.log("おわり");
 
+
+// 着火のおまじない
 const eventName = typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mouseup';
+
 document.addEventListener(eventName, initAudioContext);
+
 function initAudioContext(){
   document.removeEventListener(eventName, initAudioContext);
   // wake up AudioContext
-  console.log("きた");
   actx.resume();
 }
+
