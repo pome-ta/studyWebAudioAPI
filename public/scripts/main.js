001bc217a7ebc0a1a44ea41074b26504696d7bf5
pome-ta <pome-ta@users.noreply.github.com>
2021-05-13 10:37:19 +0900

    vs

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,23 @@
 'use strict';
 
+
+const canvas = document.querySelector('.visualizer');
+const canvasCtx = canvas.getContext("2d");
+const intendedWidth = document.querySelector('.wrapper').clientWidth;
+canvas.setAttribute('width', intendedWidth);
+
+const drawVisual;
+
+
+
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const actx = new AudioContext();
+const analyser = actx.createAnalyser();
 const osc = actx.createOscillator();
-osc.connect(actx.destination);
+
+osc.connect(analyser);
+analyser.connect(actx.destination);
+visualize()
 osc.start();
 console.log("おわり");
 
@@ -16,4 +30,38 @@ function initAudioContext(){
   // wake up AudioContext
   actx.resume();
 }
+
+
+function visualize() {
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
+  analyser.fftSize = 2048;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+  
+  const draw = function() {
+    drawVisual = requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+    canvasCtx.beginPath();
+  }
+  
+
+}
+
+
+
+function draw() {
+  
+}
+
+
+
+
+console.log(intendedWidth);
 
