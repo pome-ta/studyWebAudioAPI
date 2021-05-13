b0d606a0d298e275496cc9417cc0328f258e2542
pome-ta <pome-ta@users.noreply.github.com>
2021-05-13 13:28:24 +0900

    not run

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
-
-
+/*
+console.log("hi");
 const canvas = document.querySelector('.visualizer');
 const canvasCtx = canvas.getContext("2d");
 const intendedWidth = document.querySelector('.wrapper').clientWidth;
@@ -8,7 +8,7 @@ canvas.setAttribute('width', intendedWidth);
 
 const drawVisual;
 
-
+*/
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const actx = new AudioContext();
@@ -17,7 +17,7 @@ const osc = actx.createOscillator();
 
 osc.connect(analyser);
 analyser.connect(actx.destination);
-visualize()
+//visualize()
 osc.start();
 console.log("おわり");
 
@@ -27,12 +27,14 @@ const eventName = typeof document.ontouchend !== 'undefined' ? 'touchend' : 'mou
 document.addEventListener(eventName, initAudioContext);
 function initAudioContext(){
   document.removeEventListener(eventName, initAudioContext);
+  console.log("re");
   // wake up AudioContext
   actx.resume();
 }
 
 
 function visualize() {
+  console.log("vs");
   WIDTH = canvas.width;
   HEIGHT = canvas.height;
   analyser.fftSize = 2048;
@@ -42,26 +44,27 @@ function visualize() {
   
   const draw = function() {
     drawVisual = requestAnimationFrame(draw);
-
     analyser.getByteTimeDomainData(dataArray);
     canvasCtx.fillStyle = 'rgb(200, 200, 200)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
     canvasCtx.lineWidth = 2;
     canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
     canvasCtx.beginPath();
-  }
-  
-
+    const sliceWidth = WIDTH * 1.0 / bufferLength;
+    const x;
+    for(let i = 0; i < bufferLength; i++) {
+      const v = dataArray[i] / 128.0;
+      const y = v * HEIGHT/2;
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+      x += sliceWidth;
+    }
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.stroke();
+    
+  };
+  draw();
 }
-
-
-
-function draw() {
-  
-}
-
-
-
-
-console.log(intendedWidth);
-
