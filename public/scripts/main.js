eadd668ae88865c206d68e184b7474c2074766bd
pome-ta <pome-ta@users.noreply.github.com>
2021-05-13 15:23:18 +0900

    音は鳴った

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,13 @@
 'use strict';
-/*
+
 console.log("hi");
 const canvas = document.querySelector('.visualizer');
 const canvasCtx = canvas.getContext("2d");
 const intendedWidth = document.querySelector('.wrapper').clientWidth;
 canvas.setAttribute('width', intendedWidth);
 
-const drawVisual;
 
-*/
+
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const actx = new AudioContext();
@@ -17,7 +16,7 @@ const osc = actx.createOscillator();
 
 osc.connect(analyser);
 analyser.connect(actx.destination);
-//visualize()
+visualize()
 osc.start();
 console.log("おわり");
 
@@ -35,15 +34,15 @@ function initAudioContext(){
 
 function visualize() {
   console.log("vs");
-  WIDTH = canvas.width;
-  HEIGHT = canvas.height;
+  const WIDTH = canvas.width;
+  const HEIGHT = canvas.height;
   analyser.fftSize = 2048;
   const bufferLength = analyser.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
   canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
   
   const draw = function() {
-    drawVisual = requestAnimationFrame(draw);
+    const drawVisual = requestAnimationFrame(draw);
     analyser.getByteTimeDomainData(dataArray);
     canvasCtx.fillStyle = 'rgb(200, 200, 200)';
     canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
@@ -51,7 +50,7 @@ function visualize() {
     canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
     canvasCtx.beginPath();
     const sliceWidth = WIDTH * 1.0 / bufferLength;
-    const x;
+    let x;
     for(let i = 0; i < bufferLength; i++) {
       const v = dataArray[i] / 128.0;
       const y = v * HEIGHT/2;
