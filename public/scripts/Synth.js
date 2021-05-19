fcaaf4f8bc6d5cbcea5a11215ebfd42c8d5c8041
pome-ta <pometarou93@gmail.com>
2021-05-19 23:22:59 +0900

    js update

--- public/scripts/Synth.js
+++ public/scripts/Synth.js
@@ -8,74 +8,75 @@ export default class Synth {
     this.isPlaying;
     this.init();
   }
-  
+
   init() {
     this.isPlaying = false;
-    
+
     this.attack  = 0.5;
     this.decay   = 0.2;
     this.sustain = 0.5;
     this.release = 0.3;
-    
+
     this.auctx = new this.AudioContext();
     //this.gainNode = this.auctx.createGain();
     this.eg = this.auctx.createGain();
     this.masterVolume = this.auctx.createGain();
     this.analyzeNode = this.auctx.createAnalyser();
     this.oscNode = this.auctx.createOscillator();
-    
+
     this.oscNode.frequency.value = 220;
-    
+
     this.oscNode.connect(this.eg);
     this.eg.connect(this.masterVolume);
     this.masterVolume.connect(this.analyzeNode);
-    
+
     this.analyzeNode.connect(this.auctx.destination);
-    
+
     this.t0 = this.auctx.currentTime;
     this.t1 = this.t0 + this.attack;
     //this.eg.gain.value = 0;
     //this.eg.gain.setTargetAtTime(1, this.t0, this.t1);
     this.eg.gain.linearRampToValueAtTime(1, this.t1);
-    
+
     this.t2 = this.decay;
     this.t2Value = this.sustain;
     this.eg.gain.setTargetAtTime(this.t2Value, this.t1, this.t2);
     this.t3 = this.auctx.currentTime;
     this.t4 = this.release;
     this.eg.gain.setTargetAtTime(0, this.t3, this.t4);
-    
-    
+
+
   }
-  
-  
-  
-  play() {
+
+
+
+  play(attack, decay, sustain) {
     this.init();
     if (this.isPlaying) {
       this.oscNode.stop(0);
     }
+    this.attack = attack;
+    this.decay = decay;
+    this.sustain = sustain;
     //this.eg.gain.setTargetAtTime(1, this.t0, this.t1);
     this.t0 = this.auctx.currentTime;
-    
-    
     this.t1 = this.t0 + this.attack;
     this.t2 = this.decay;
     this.t2Value = this.sustain;
     this.eg.gain.linearRampToValueAtTime(1, this.t1);
     this.eg.gain.setTargetAtTime(this.t2Value, this.t1, this.t2);
-    
+
     this.oscNode.start(this.t0);
-    
-    
+
     this.isPlaying = true;
     //console.log(typeof this.eg.gain.value);
   }
-  
-  end() {
+
+  end(release) {
     if (!this.isPlaying) {
       return;
     }
+    this.release = release;
     this.t3 = this.auctx.currentTime;
     this.t4 = this.release;
     this.eg.gain.cancelScheduledValues(this.t3);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,5 @@
 import Synth from './Synth.js';
 
-
-
 const {tapStart, tapMove, tapEnd} = {
   tapStart: typeof document.ontouchstart !== 'undefined' ? 'touchstart' : 'mousedown',
   tapMove: typeof document.ontouchmove !== 'undefined' ? 'touchmove' : 'mousemove',
@@ -32,53 +30,80 @@ const sValue = document.querySelector('#s-param');
 const rValue = document.querySelector('#r-param');
 
 
-
+// todo : python のzip 的なのを考えてたい
 const setControls = [volControl, frqControl, aControl, dControl, sControl, rControl];
 const setValues = [volValue, frqValue, aValue, dValue, sValue, rValue];
+
+// 初期化
+(function (rng, prm) {
+  for (let i=0; i < rng.length; i++) {
+    prm[i].innerHTML = rng[i].value;
+  }
+}(setControls, setValues));
+
 
 
 
 
 toneControl.addEventListener(tapStart, (e) => {
   e.preventDefault();
-  synth.play();
+  synth.play(0,0,0);
 });
 
 toneControl.addEventListener(tapEnd, () => {
-  synth.end();
+  synth.end(0);
 });
 
 volControl.addEventListener('input', function() {
   synth.masterVolume.gain.setValueAtTime(this.value, synth.auctx.currentTime);
-  
+  volValue.innerHTML = this.value;
 });
 
 frqControl.addEventListener('input', function() {
   synth.oscNode.frequency.setValueAtTime(this.value, synth.auctx.currentTime);
+  frqValue.innerHTML = this.value;
 });
 
+
+
+aControl.addEventListener('input', function() {
+  // synth.oscNode.frequency.setValueAtTime(this.value, synth.auctx.currentTime);
+  aValue.innerHTML = this.value;
+});
+dControl.addEventListener('input', function() {
+  // synth.oscNode.frequency.setValueAtTime(this.value, synth.auctx.currentTime);
+  dValue.innerHTML = this.value;
+});
+sControl.addEventListener('input', function() {
+  // synth.oscNode.frequency.setValueAtTime(this.value, synth.auctx.currentTime);
+  sValue.innerHTML = this.value;
+});
+rControl.addEventListener('input', function() {
+  // synth.oscNode.frequency.setValueAtTime(this.value, synth.auctx.currentTime);
+  rValue.innerHTML = this.value;
+});
 
 /* visualizar */
 function visualize() {
   const WIDTH = viCanvas.width;
   const HEIGHT = viCanvas.height;
-  
+
   synth.analyzeNode.fftSize = 2048;
   const bufferLength = synth.analyzeNode.frequencyBinCount;
   const dataArray = new Uint8Array(bufferLength);
   vcctx.clearRect(0, 0, WIDTH, HEIGHT);
-  
+
   const draw = () => {
     requestAnimationFrame(draw);
     synth.analyzeNode.getByteTimeDomainData(dataArray);
-    
+
     vcctx.fillStyle = 'rgb(233, 233, 233)';
     vcctx.fillRect(0, 0, WIDTH, HEIGHT);
     vcctx.lineWidth = 1;
     vcctx.strokeStyle = 'rgb(35, 35, 35)';
     vcctx.beginPath();
     const sliceWidth = WIDTH * 1.0 / bufferLength;
-    
+
     let x = 0;
     for (let i = 0; i < bufferLength; i++) {
       const v = dataArray[i] / 128.0;
@@ -89,7 +114,7 @@ function visualize() {
     }
     vcctx.lineTo(viCanvas.width, viCanvas.height / 2);
     vcctx.stroke();
-    
+
   };
   draw();
 }
