684dd9ead35c15df31a7e78edff027a5d1ed678a
pome-ta <pome-ta@users.noreply.github.com>
2021-05-26 09:53:27 +0900

    analyzer

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,11 +6,12 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 const context = new AudioContext();
 
 
-const intendedWidth = document.querySelector('.synth').clientWidth;
+const keyboardWidth = document.querySelector('.synth').clientWidth;
+
 const settings = {
   id: 'keyboard',
-  width: `${intendedWidth}`,
-  height: 150,
+  width: `${keyboardWidth}`,
+  height: 128,
   startNote: 'A2',
   margin: 'auto',
   whiteNotesColour: '#fff',
@@ -33,8 +34,12 @@ keyboard.keyDown = (_, frequency) => {
   const oscillator = context.createOscillator();
   oscillator.type = 'square';
   oscillator.frequency.value = frequency;
-  oscillator.connect(masterGain);
+  const analyzeNode = context.createAnalyser();
+  oscillator.connect(analyzeNode);
+  analyzeNode.connect(masterGain);
   oscillator.start(0);
+  visualize(analyzeNode);
+  
 
   nodes.push(oscillator);
 };
@@ -58,3 +63,51 @@ keyboard.keyUp = (_, frequency) => {
 
 // xxx: 🤔
 keyboard = new window.QwertyHancock(settings);
+
+
+
+
+/* visualizar */
+function visualize(analyzer) {
+  const WIDTH = viCanvas.width;
+  const HEIGHT = viCanvas.height;
+
+  analyzer.fftSize = 2048;
+  const bufferLength = analyzer.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  vcctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  const draw = () => {
+    requestAnimationFrame(draw);
+    analyzer.getByteTimeDomainData(dataArray);
+
+    vcctx.fillStyle = 'rgb(233, 233, 233)';
+    vcctx.fillRect(0, 0, WIDTH, HEIGHT);
+    vcctx.lineWidth = 1;
+    vcctx.strokeStyle = 'rgb(35, 35, 35)';
+    vcctx.beginPath();
+    const sliceWidth = WIDTH * 1.0 / bufferLength;
+
+    let x = 0;
+    for (let i = 0; i < bufferLength; i++) {
+      const v = dataArray[i] / 128.0;
+      const y = v * HEIGHT / 2;
+      // todo: ショートハンドすぎる？
+      i === 0 ? vcctx.moveTo(x, y) : vcctx.lineTo(x, y);
+      x += sliceWidth;
+    }
+    vcctx.lineTo(viCanvas.width, viCanvas.height / 2);
+    vcctx.stroke();
+
+  };
+  draw();
+}
+
+const viCanvas = document.querySelector('.visualizer');
+const vcctx = viCanvas.getContext("2d");
+const intendedWidth = document.querySelector('.wrapper').clientWidth;
+viCanvas.setAttribute('width', intendedWidth);
+viCanvas.setAttribute('height', intendedWidth / 4);
+
+
+
