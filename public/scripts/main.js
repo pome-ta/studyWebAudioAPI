3d0236f40085c2d2c9ce6a9e74b20cff8397e7a7
pome-ta <pome-ta@users.noreply.github.com>
2021-05-27 15:42:23 +0900

    戻した

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,68 +25,36 @@ const settings = {
 let keyboard = new window.QwertyHancock(settings);
 
 const masterGain = context.createGain();
-//const analyzeNode = context.createAnalyser();
-
-/* VCO */
-const vco = context.createOscillator();
-vco.type = 'square';
-//vco.frequency.value = frequency;
-
-
-/* VCA */
-const vca = context.createGain();
-vca.gain.value = 0;
-
-
-
-/* Connections */
-vco.connect(vca);
-vca.connect(masterGain);
-//vca.connect(analyzeNode);
-//analyzeNode.connect(masterGain);
-
-
-
-
-//analyzeNode.connect(masterGain);
+let nodes = [];
 
 masterGain.gain.value = 0.3;
 masterGain.connect(context.destination);
 
-
-vco.start(0);
-
-
 keyboard.keyDown = (_, frequency) => {
-  vco.frequency.value = frequency;
-  vca.gain.value = 1;
-};
-
-keyboard.keyUp = (_, _) => {
-  vca.gain.value = 0;
-};
-
-/*
-let nodes = [];
-
-
-
-keyboard.keyDown = (note, frequency) => {
+  const oscillator = context.createOscillator();
+  oscillator.type = 'square';
+  oscillator.frequency.value = frequency;
+  const analyzeNode = context.createAnalyser();
+  oscillator.connect(analyzeNode);
+  analyzeNode.connect(masterGain);
+  oscillator.start(0);
+  visualize(analyzeNode);
   
+
   nodes.push(oscillator);
 };
 
 
 
-keyboard.keyUp = (note, frequency) => {
+keyboard.keyUp = (_, frequency) => {
   const newNodes = [];
-  
-  for (const nd of nodes) {
-    if (Math.round(nd.frequency.value) === Math.round(frequency)) {
-      nd.stop(0);
-      nd.disconnect();
+
+  for (let i = 0; i < nodes.length; i++) {
+    if (Math.round(nodes[i].frequency.value) === Math.round(frequency)) {
+      nodes[i].stop(0);
+      nodes[i].disconnect();
     } else {
-      newNodes.push(nd);
+      newNodes.push(nodes[i]);
     }
   }
 
@@ -96,11 +64,10 @@ keyboard.keyUp = (note, frequency) => {
 // xxx: 🤔
 keyboard = new window.QwertyHancock(settings);
 
-*/
+
 
 
 /* visualizar */
-/*
 function visualize(analyzer) {
   const WIDTH = viCanvas.width;
   const HEIGHT = viCanvas.height;
@@ -142,5 +109,4 @@ const intendedWidth = document.querySelector('.wrapper').clientWidth;
 viCanvas.setAttribute('width', intendedWidth);
 viCanvas.setAttribute('height', intendedWidth / 4);
 
-*/
-
+ 
