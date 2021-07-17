fb56bf9c8d09ef4297ff302597b2f6a9856f8a8e
pome-ta <pome-ta@users.noreply.github.com>
2021-07-17 22:40:55 +0900

    out module

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,5 @@
-'use strict';
-
+import {wavVisualize} from './visualize.js';
+import {barVisualize} from './visualize.js';
 
 const keyboardWidth = document.querySelector('.synth').clientWidth;
 
@@ -81,88 +81,6 @@ keyboard.keyUp = (note, frequency) => {
 
 // xxx: 🤔
 //keyboard = new window.QwertyHancock(settings);
-
-
-function wavVisualize(canvasTag, analyze) {
-  const vcctx = canvasTag.getContext("2d");
-  const intendedWidth = document.querySelector('.wrapper').clientWidth;
-  canvasTag.setAttribute('width', intendedWidth);
-  canvasTag.setAttribute('height', intendedWidth / 3)
-  const WIDTH = canvasTag.width;
-  const HEIGHT = canvasTag.height;
-  
-  const wavAnalyze = analyze;
-
-  wavAnalyze.fftSize = 2048;
-  const bufferLength = wavAnalyze.fftSize;
-  const dataArray = new Uint8Array(bufferLength);
-  vcctx.clearRect(0, 0, WIDTH, HEIGHT);
-
-  draw();
-  function draw() {
-    requestAnimationFrame(draw);
-    wavAnalyze.getByteTimeDomainData(dataArray);
-
-    vcctx.fillStyle = 'rgb(233, 233, 233)';
-    vcctx.fillRect(0, 0, WIDTH, HEIGHT);
-    vcctx.lineWidth = 1;
-    vcctx.strokeStyle = 'rgb(35, 35, 35)';
-    vcctx.beginPath();
-    const sliceWidth = WIDTH * 1.0 / bufferLength;
-
-    let x = 0;
-    for (let i = 0; i < bufferLength; i++) {
-      const v = dataArray[i] / 128.0;
-      const y = v * HEIGHT / 2;
-      // todo: ショートハンドすぎる？
-      i === 0 ? vcctx.moveTo(x, y) : vcctx.lineTo(x, y);
-      x += sliceWidth;
-    }
-    vcctx.lineTo(canvasTag.width, canvasTag.height / 2);
-    vcctx.stroke();
-  };
-}
-
-
-function barVisualize(canvasTag, analyze) {
-  const vcctx = canvasTag.getContext("2d");
-  const intendedWidth = document.querySelector('.wrapper').clientWidth;
-  canvasTag.setAttribute('width', intendedWidth);
-  canvasTag.setAttribute('height', intendedWidth / 3)
-  const WIDTH = canvasTag.width;
-  const HEIGHT = canvasTag.height;
-  const barAnalyze = analyze;
-  barAnalyze.fftSize = 512;
-
-  const bufferLengthAlt = barAnalyze.frequencyBinCount;
-  console.log(bufferLengthAlt);
-  
-
-  const dataArrayAlt = new Uint8Array(bufferLengthAlt);
-  vcctx.clearRect(0, 0, WIDTH, HEIGHT);
-  
-  drawAlt();
-  function drawAlt() {
-    requestAnimationFrame(drawAlt);
-    
-    barAnalyze.getByteFrequencyData(dataArrayAlt);
-    
-    vcctx.fillStyle = 'rgb(233, 233, 233)';
-    vcctx.fillRect(0, 0, WIDTH, HEIGHT);
-    
-    const barWidth = (WIDTH / bufferLengthAlt) * 2.5;
-    
-    let barHeight;
-    let x = 0;
-
-    for (let i = 0; i < bufferLengthAlt; i++) {
-      barHeight = dataArrayAlt[i];
-      vcctx.fillStyle = `rgb(${barHeight+64}, 64, 64)`;
-      vcctx.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
-      x += barWidth + 1;
-    }
-  };
-}
 
 
 
